c4407e1bf821e25164c8804ae94521ca0f9f090c
supspec <supspec@gmail.com>
2018-01-12 04:47:31 +0700

    bittrexgetmarkets

--- check.php
+++ check.php
@@ -1,7 +1,15 @@
 <?php
 
-$bitrexFee = 0.25;
-$poloniexFee = 0.25; // Maker
+$bitrexFee = 0.25; //%
+$poloniexFee = 0.25; //% // Maker
+$BtxBtcFee = 0.001; // btc
+$PolBtcFee = 0.0005;// btc
+
+/*
+1. bittrexBuy (bitrexFee) -> bittrexWithdraw (txFee) -> poloniexSell (poloniexFee) -> poloniexWithdraw (PolBtcFee)
+2. poloniexBuy (poloniexFee) -> poloniexWithdraw (txFee) -> bittrexSell (bitrexFee) -> bittrexWithdraw (BtxBtcFee)
+*/
+
 
 function getBittrexCoins(){
 	$data = json_decode(file_get_contents("https://bittrex.com/api/v1.1/public/getcurrencies"));
@@ -30,6 +38,46 @@ function getPoloniexCoins(){
 	return $result;
 }
 
-var_dump(getPoloniexCoins());
+
+function getPoloniexMarkets(){
+	$data = json_decode(file_get_contents("https://poloniex.com/public?command=returnTicker"));
+	$result = array();
+	$coins = getPoloniexCoins();
+	foreach($data as $k=>$v){
+		$keys = split("_", $k);
+		if(!$v->isFrozen && $keys[0] == 'BTC' && isset($coins[$keys[1]]) ){
+			$result[$keys[1]]['market'] = $k;
+			$result[$keys[1]]['txFee'] = $coins[$keys[1]];
+			$result[$keys[1]]['last'] = $v->last;
+			$result[$keys[1]]['volume'] = $v->baseVolume;
+			$result[$keys[1]]['spred'] = $v->percentChange;
+		}
+	}
+	
+	return $result;
+}
+
+function getBittrexMarkets(){
+	$data = json_decode(file_get_contents("https://bittrex.com/api/v1.1/public/getmarketsummaries"));
+	$result = array();
+	$coins = getBittrexCoins();
+	foreach($data->result as $market){
+		$name = split('-', $market->MarketName);
+		//$p = split('-', $name);
+		if($name[0] == 'BTC' && isset($coins[$name[1]])){
+			$result[$name[1]]['market'] = $market->MarketName;
+			$result[$name[1]]['txFee'] = $coins[$name[1]];
+			$result[$name[1]]['last'] = $market->Last;
+			$result[$name[1]]['volume'] = $market->BaseVolume;
+			//$result[$name[1]]['spred'] = $v->percentChange;
+		}
+	}
+	
+	return $result;	
+	
+}
+
+
+var_dump(getBittrexMarkets());
 
 //var_dump(getBittrexCoins());
